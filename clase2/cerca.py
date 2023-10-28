c74a93d29350e82e900bf82f839f2dbe1da722e4
Petter Axcell <petteraxcell03@gmail.com>
2023-10-28 09:43:05 +0200

    cerca de la primera vocal

--- clase2/cerca.py
+++ clase2/cerca.py
@@ -1,8 +1,24 @@
-def return_number_consonants():
-    pass
+def return_number_consonants(string1):
+    '''
+        Lo que hace
+        Argumentos:
+            String: Una palabra
+        Return:
+            Int: La posición de la primera vocal
+    '''
+    i = 0
+    flag = True
+    while(flag):
+        if string1[i] in 'aeiouAEIOU':
+            flag = False
+        else:
+            i+=1
+    return i
+
 
 def cerca():
-    pass
+    string1 = "brasa"
+    return_number_consonants(string1)
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     cerca()
