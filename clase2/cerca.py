12f9dfb319e31dad0fc8127acd53b214e7746033
Petter Axcell <petteraxcell03@gmail.com>
2023-10-28 10:02:18 +0200

    return string longer

--- clase2/cerca.py
+++ clase2/cerca.py
@@ -1,4 +1,4 @@
-def return_number_consonants(string1):
+def return_position_firs_vowal(string1):
     '''
         Lo que hace
         Argumentos:
@@ -16,9 +16,31 @@ def return_number_consonants(string1):
     return i
 
 
+def return_string_longer(string1, string2):
+    '''
+        Esta función devuelve el parametro más largo
+        Args:
+            string: Primer string
+            string: Segundo string
+        Returns:
+            String: Longer string
+    '''
+    i = 0
+    while(i<len(string1) and i<len(string2)):
+        print(string1[i])
+        print(string2[i])
+        i+=1
+
+    if(i==len(string1)):
+        return string2
+    else:
+        return string1
+
 def cerca():
     string1 = "brasa"
-    return_number_consonants(string1)
+    string2 = "Voy al parque a jugar"
+    string_longer = return_string_longer(string1, string2)
+    print(string_longer)
 
 if __name__ == '__main__':
     cerca()
