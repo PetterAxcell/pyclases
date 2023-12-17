79642e56d946476646e330dc3aecb34572cf4e8c
Petter Axcell <petteraxcell03@gmail.com>
2023-12-17 15:33:54 +0100

    explanation

---
+++ clase5/index4.py
@@ -0,0 +1,17 @@
+diccionario1 = {"Pepa":{"Dni":"3453420H", "Codigo Postal": "094357"}}
+#db = {...}
+db = {"3453420H":{"Name": "Pepa", "Código Postal": "094357"}}
+
+#for key in list(diccionario1.keys()):
+#    if key not in db:
+#        db[key] = diccionario1[key] 
+#print(db)
+
+
+key = list(diccionario1.keys())
+
+print(key)
+
+print(key[0])
+
+
